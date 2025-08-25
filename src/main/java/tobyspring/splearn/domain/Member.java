ac5db894efe39e21694271b4dcea6a7c4d670c90
mingdodev <mingdo.dev@gmail.com>
2025-08-25 16:38:55 +0900

    Memeber 도메인 로직 개발

--- src/main/java/tobyspring/splearn/domain/Member.java
+++ src/main/java/tobyspring/splearn/domain/Member.java
@@ -1,5 +1,13 @@
 package tobyspring.splearn.domain;
 
+import static org.springframework.util.Assert.state;
+
+import java.util.Objects;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
 public class Member {
     private String email;
 
@@ -10,25 +18,21 @@ public class Member {
     private MemberStatus status;
 
     public Member(String email, String nickname, String passwordHash) {
-        this.email = email;
-        this.nickname = nickname;
-        this.passwordHash = passwordHash;
+        this.email = Objects.requireNonNull(email);
+        this.nickname = Objects.requireNonNull(nickname);
+        this.passwordHash = Objects.requireNonNull(passwordHash);
         this.status = MemberStatus.PENDING;
     }
 
-    public String getEmail() {
-        return email;
+    public void activate() {
+        state(status == MemberStatus.PENDING, "PENDING 상태가 아닙니다");
+
+        this.status = MemberStatus.ACTIVATE;
     }
 
-    public MemberStatus getStatus() {
-        return status;
-    }
+    public void deactivate() {
+        state(status == MemberStatus.ACTIVATE, "ACTIVE 상태가 아닙니다");
 
-    public String getPasswordHash() {
-        return passwordHash;
-    }
-
-    public String getNickname() {
-        return nickname;
+        this.status = MemberStatus.DEACTIVATED;
     }
 }
--- src/test/java/tobyspring/splearn/domain/MemberTest.java
+++ src/test/java/tobyspring/splearn/domain/MemberTest.java
@@ -1,6 +1,7 @@
 package tobyspring.splearn.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
 
@@ -10,5 +11,51 @@ class MemberTest {
         var member = new Member("user@example.com", "user", "secret");
 
         assertThat(member.getStatus()).isEqualTo(MemberStatus.PENDING);
+    }
+
+    @Test
+    void 생성자에서_Null이_들어오면_예외가_발생() {
+        assertThatThrownBy(() -> new Member(null, "user", "secret"))
+                .isInstanceOf(NullPointerException.class);
+    }
+
+    @Test
+    void 회원_활성화() {
+        var member = new Member("user@example.com", "user", "secret");
+
+        member.activate();
+
+        assertThat(member.getStatus()).isEqualTo(MemberStatus.ACTIVATE);
+    }
+
+    @Test
+    void 회원_활성화_실패() {
+        var member = new Member("user@example.com", "user", "secret");
+
+        member.activate();
+
+        assertThatThrownBy(member::activate)
+                .isInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    void 회원_탈퇴() {
+        var member = new Member("user@example.com", "user", "secret");
+
+        member.activate();
+        member.deactivate();
+
+        assertThat(member.getStatus()).isEqualTo(MemberStatus.DEACTIVATED);
+    }
+
+    @Test
+    void 회원_탈퇴_실패() {
+        var member = new Member("user@example.com", "user", "secret");
+
+        member.activate();
+        member.deactivate();
+
+        assertThatThrownBy(member::deactivate)
+                .isInstanceOf(IllegalStateException.class);
     }
 }
