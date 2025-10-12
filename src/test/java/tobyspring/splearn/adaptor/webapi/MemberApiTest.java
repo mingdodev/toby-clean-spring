42b4930d6f9574d903bdbe71610a1454e13fd8df
mingdodev <mingdo.dev@gmail.com>
2025-10-12 22:01:10 +0900

    MemberApi 웹 단위 테스트

---
+++ src/test/java/tobyspring/splearn/adaptor/webapi/MemberApiTest.java
@@ -0,0 +1,64 @@
+package tobyspring.splearn.adaptor.webapi;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import net.bytebuddy.matcher.MethodExceptionTypeMatcher;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.assertj.MockMvcTester;
+import tobyspring.splearn.application.member.provided.MemberRegister;
+import tobyspring.splearn.domain.member.Member;
+import tobyspring.splearn.domain.member.MemberFixture;
+import tobyspring.splearn.domain.member.MemberRegisterRequest;
+
+@WebMvcTest(MemberApi.class)
+@RequiredArgsConstructor
+class MemberApiTest {
+    // record의 생성자 주입은 클래스 인스턴스 생성 시점에 빈을 찾음. 특히 본질은 불변성.
+    // MockBean은 스프링이 띄워진 후 해당 타입의 실제 빈을 찾아서 가짜 객체로 바꿔치기 함.
+    // 이 동작들은 시점 자체가 충돌: 모키토가 바꿔치기 하기 전에 이미 테스트 클래스는 진짜 빈을 주입하여 생성됨
+    @MockitoBean
+    private MemberRegister memberRegister;
+
+    // @MockBean이 붙은 필드를 생성자에서 제외했기 때문에 동작. 반면, record는 모든 필드를 생성자에 포함시켜야 하므로 이런 분리가 불가능
+    final MockMvcTester mvcTester;
+    final ObjectMapper objectMapper;
+
+    @Test
+    void register() throws JsonProcessingException {
+        Member member = MemberFixture.createMember(1L);
+        when(memberRegister.register(any())).thenReturn(member);
+
+        MemberRegisterRequest request = MemberFixture.createMemberRegisterRequest();
+        String requestJson = objectMapper.writeValueAsString(request);
+
+        assertThat(mvcTester.post().uri("/api/members").contentType(MediaType.APPLICATION_JSON)
+                .content(requestJson))
+                .hasStatusOk()
+                .bodyJson()
+                .extractingPath("$.memberId").asNumber().isEqualTo(1);
+
+        verify(memberRegister).register(request);
+    }
+
+    @Test
+    void registerFail() throws JsonProcessingException {
+        MemberRegisterRequest request = MemberFixture.createMemberRegisterRequest("invalid email");
+        String requestJson = objectMapper.writeValueAsString(request);
+
+        assertThat(mvcTester.post().uri("/api/members").contentType(MediaType.APPLICATION_JSON)
+                .content(requestJson))
+                .hasStatus(HttpStatus.BAD_REQUEST);
+    }
+}
